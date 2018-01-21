7cfbe4550968a3a8947bdc2f8409f47a2a70394f
AlekseyLeshko <alekseyleshko@gmail.com>
2018-01-21 23:29:42 +0400

    Add node_env in store

--- react-redux-with-entity-model/src/default/store/index.js
+++ react-redux-with-entity-model/src/default/store/index.js
@@ -2,8 +2,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../modules/rootReducer.js';
 
-// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-const store = createStore(reducer);
+console.log(process.env.NODE_ENV);
+const store = process.env.NODE_ENV === 'test' ? createStore(reducer) : createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
 
