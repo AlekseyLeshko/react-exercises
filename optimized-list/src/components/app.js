f3afbaef4b3b7bb5f441262615ab797d7aa23bc0
AlekseyLeshko <alekseyleshko@gmail.com>
2018-02-20 23:48:08 +0400

    Add why-did-you-update plugin

--- optimized-list/src/components/app.js
+++ optimized-list/src/components/app.js
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import Main from './main';
 
+const {whyDidYouUpdate} = require('why-did-you-update');
+whyDidYouUpdate(React);
+
 const app = () => (
   <Provider store={store}>
     <Main />
