1f6012fdf4a2befaccdfccaa0e4240d323280a69
AlekseyLeshko <alekseyleshko@gmail.com>
2018-02-21 18:23:00 +0400

    Rework header

--- optimized-list/src/components/header/index.js
+++ optimized-list/src/components/header/index.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import usersContainer from '../../containers/users';
-import RandomButton from './components/random-toggle';
 import {getName} from '../../helper';
 require('./style.css');
 
-class HeaderComponent extends Component {
+class Header extends PureComponent {
   addUser = () => {
     const user = {
       name: getName(),
@@ -18,13 +17,12 @@ class HeaderComponent extends Component {
     return (
       <div className="header">
         <button onClick={this.addUser}>Add user</button>
-        <RandomButton />
       </div>
     );
   }
 }
 
-HeaderComponent.defaultProps = {
+Header.defaultProps = {
 };
 
-export default usersContainer(HeaderComponent);
+export default usersContainer(Header);
